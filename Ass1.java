91ef7a2b141248752622a9b5bfe9f9b8fd3f048c
naveengoudamk <navi112004naveen@gmail.com>
2026-01-30 00:03:18 +0530

    Initial java codes of day first only printing codes

---
+++ Ass1.java
@@ -0,0 +1,12 @@
+class Ass1 
+{
+    public static void main(String [] args)
+    {
+        System.out.println("My name is Naveengouda ");
+        System.out.println("My father name is Mallanagouda ");
+        System.out.println("My Mother name is Malakka ");
+        System.out.println("My Brother name is Naganagouda ");
+
+    }
+    
+}
