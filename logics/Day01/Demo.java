680886b4a106a90f1ce7535d0334bd31d1153bdd
naveengoudamk <navi112004naveen@gmail.com>
2026-02-12 23:26:08 +0530

    Audio and video  examples with concept

---
+++ logics/Day01/Demo.java
@@ -0,0 +1,17 @@
+class Demo 
+{
+    public static void main(String [] args){
+
+    
+    int num = 12 ;
+    if( num % 2 == 0)
+        {
+            System.out.println("Num is Even " );
+        }
+    }    
+}
+class demo2
+{
+    
+
+}
